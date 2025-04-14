fed573f6c15217813aa59fdc9bd57070ddf838bd
Varshini Byri <145209852+ByriVarshini@users.noreply.github.com>
2025-04-14 15:58:18 +0530

    Update settings.py

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-d63h_j+z(+^7@fgn2^*$!9b&l5l4vcqrn+9t5t@*%vuxpdg2n6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'my-task-flow-one.vercel.app',
+]
 
 
 # Application definition
